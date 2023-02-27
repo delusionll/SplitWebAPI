edbd26c0059439fc0f1110101ff242a3a877f91e
delusionll <lev.zernov@yandex.ru>
2023-02-27 20:58:01 +0300

    code review

--- BLL/IBLL.cs
+++ BLL/IBLL.cs
@@ -7,16 +7,29 @@ namespace SplitWebAPI.BLL
     {
         public static void CountExpense(double amount, int userIdFrom, List<Benefiter> benefitersList) //Учитывает трату, меняет баланс в базе
         {
-            using(SplitContext _db = new())
+            using (SplitContext _db = new())
             {
-                User userFrom = _db.Users.Find(userIdFrom);
-                userFrom.UserBalance += amount;
-                foreach(Benefiter b in benefitersList)
                 {
-                    User userToBenefit = _db.Users.Find(b.UserId);
-                    userToBenefit.UserBalance -= amount * b.BenefiterPercent/100;
+                    User userFrom = _db.Users.Find(userIdFrom);
+                    userFrom.UserBalance += amount;
+                    double totalPercent = 0;
+                    foreach (Benefiter b in benefitersList)
+                    {
+                        User userToBenefit = _db.Users.Find(b.UserId);
+                        userToBenefit.UserBalance -= amount * b.BenefiterPercent / 100;
+                        totalPercent += b.BenefiterPercent;
+                    }
+                    if (totalPercent == 100)
+                    {
+                        _db.SaveChanges();
+
+                    }
+                    else
+                    {
+                        throw new ArgumentException("wrong percent Sum");
+                    }
+
                 }
-                _db.SaveChanges();
             }
         }
     }
